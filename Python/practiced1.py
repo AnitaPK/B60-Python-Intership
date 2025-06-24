b3cc2ca6d7ea72f0d98a8d29cb0e19d0f1486669
AnitaPK <anita.khamitkar@gmail.com>
2025-06-24 17:29:48 +0530

    python started

---
+++ Python/practiced1.py
@@ -0,0 +1,10 @@
+print("Hello world")
+
+name1 = input("Enter name")
+print("Hello",name1)
+
+name = input("Enter name")
+age = input("Enter age")
+country = input("Enter country ")
+
+print("name", name, " age ", age, " country " , country)
